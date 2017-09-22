4338926a0f3848edb75eb1d23aa76d5c56404b59
chandlerto <32106460+chandlerto@users.noreply.github.com>
2017-09-22 12:02:49 -0500

    Brainstorm/Outline
    Discussed how program should take user input in, what values are needed, and evaluated what special cases and issues should be handled by the program or left to the user to sort through.

---
+++ KenKen_Calculator.adb
@@ -0,0 +1,21 @@
+--KenKen_Calculator
+--Tells user possible combinations for a given KenKen set
+--Created by Chandler To
+--Started 9/22/17
+--In Progress
+
+--First, asks user size of puzzle to determine max value
+   --Have some sort of way to exit calculator when user changes to new puzzle of dif size
+--Should ask for all values at once? Or separate?
+--Values are
+   --operator (+,-,*,/) (Perhaps use numbered menu)
+   --Number of Spaces (For now, subtraction and division default to 2)
+      --Also, not accounting for if set is just one dimensional or two
+      --Should first print no duplicate solutions
+      --Then put a blank line in between showing duplicate solutions
+   --Value of set
+
+--Printing answers out may require an array?
+   --Program could just print out solutions as it finds them
+   --Can organize this so that the duplicate solutions are found in separate loop
+      
